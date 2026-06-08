274e27a593099f61b5a12a00e7556c5c418da199
PixelSaver <stephen.k@proton.me>
2026-06-07 19:41:58 -0700

    Added some more files and tried some cleaning up o fcode

--- utilities/health_component.gd
+++ utilities/health_component.gd
@@ -1,6 +1,12 @@
-class_name HealthComponent extends Node
+extends Node
+## Component to store health, takes damage using the Attack class.
+## Also applies knockback, and can be resisted if the target node
+## has the 'get_knockback_resistance' function
+class_name HealthComponent 
 
+## Emitted when health changes
 signal health_changed(health, max_health)
+## Emitted on death
 signal death()
 
 @export var health : float = 10.0 :
@@ -13,6 +19,9 @@ signal death()
 		if val != max_health:
 			max_health = val
 			health_changed.emit(health, max_health)
+## The node (player/enemy) to own this health bar. If this is
+## not set, the node will automatically use it's direct parent
+## if it's a RigidBody2D
 @export var target : RigidBody2D
 var _target : RigidBody2D
 
@@ -36,17 +45,21 @@ func damage(atk: Attack):
 
 func _process_pending_damage() -> void:
 	var tot_damage := 0.0
-	
 	for atk in _pending_atks:
 		tot_damage += atk.damage
-		if _target.has_node(^"KnockbackComponent"):
-			var kb_c = _target.get_node(^"KnockbackComponent") as KnockbackComponent
-			kb_c.apply_knockback(atk)
+		apply_knockback(atk)
 	_pending_atks.clear()
 	_processing = false
 	health -= tot_damage
 	if health <= 0 or is_equal_approx(health, 0.0):
 		death.emit()
+
+## Applies knockback unless target has the 'get_knockback_resistance' function
+func apply_knockback(atk:Attack):
+	var kb_res := 0.0
+	if _target.has_method("get_knockback_resistance"):
+		kb_res = _target.knockback_resistance
+	_target.apply_central_impulse(atk.knockback * (1-kb_res))
 
 func heal(delta:float) -> float:
 	health += delta
