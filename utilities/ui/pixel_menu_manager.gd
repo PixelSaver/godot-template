7efaf1b60318be72e8797ce6d97bb8d11603e2c2
PixelSaver <stephenk.us@proton.me>
2026-06-16 20:49:12 -0700

    Added a menu manager skeleton

---
+++ utilities/ui/pixel_menu_manager.gd
@@ -0,0 +1,16 @@
+extends Control 
+class_name PixelMenuManager
+
+enum MenuManagerState {
+	## There is a single menu existing
+	SINGLE,
+	## There is one menu going through it's end animation
+	TRANSITIONING_AWAY,
+	## There is one menu going through it's start animation
+	TRANSITIONING_TOWARDS,
+	## There are two menus, one ending, one starting
+	TRANSITIONING_BOTH,
+}
+var state : MenuManagerState = MenuManagerState.SINGLE
+var current_scene : PixelMenu
+var previous_scene : PixelMenu
